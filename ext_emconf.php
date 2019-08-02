b4667f68e30ea995cde2e72b8030d5b9ad96fb96
Matthias Stegmann <matthias.stegmann@b13.com>
2019-08-02 21:52:06 +0200

    [TASK] Update dependencies for TYPO3 v10

--- ext_emconf.php
+++ ext_emconf.php
@@ -26,7 +26,7 @@ $EM_CONF[$_EXTKEY] = array (
   array (
     'depends' => 
     array (
-      'typo3' => '7.6.0-9.9.99',
+      'typo3' => '7.6.0-10.9.99',
     ),
     'conflicts' => 
     array (
