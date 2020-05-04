6c5f1f6628f82ff9c8d1e072ef62ce1066cf5de6
Achim Fritz <achim.fritz@b13.de>
2020-05-04 17:06:20 +0200

    [TASK] use ModifyLoadedPageTsConfigEvent to load Page TS Config from Site Extension

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,11 +3,3 @@ defined('TYPO3_MODE') or die();
 
 // Register autoloading for TypoScript
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['Core/TypoScript/TemplateService']['runThroughTemplatesPostProcessing']['bolt'] = \B13\Bolt\TypoScript\Loader::class . '->addSiteConfiguration';
-
-// Register autoloading of pageTSconfig
-\TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\SignalSlot\Dispatcher::class)->connect(
-    \TYPO3\CMS\Backend\Utility\BackendUtility::class,
-    'getPagesTSconfigPreInclude',
-    B13\Bolt\TsConfig\Loader::class,
-    'addSiteConfiguration'
-);
