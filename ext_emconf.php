3ce87525872e11c1a53e59808e3c3015c06fc212
Benni Mack <benjamin.mack@b13.com>
2020-07-20 12:39:38 +0200

    [BUGFIX] Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,17 +4,17 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'Bolt - An easy TYPO3 integration basis',
     'description' => 'This package ships best defaults for integrators',
     'category' => 'fe',
-    'version' => '2.0.0',
+    'version' => '2.0.2',
     'state' => 'stable',
     'uploadfolder' => false,
     'createDirs' => '',
     'clearcacheonload' => true,
     'author' => 'Benni Mack',
-    'author_email' => '',
-    'author_company' => '',
+    'author_email' => 'typo3@b13.com',
+    'author_company' => 'b13 GmbH',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0',
+            'typo3' => '10.4.0-10.4.99',
         ]
     ]
 ];
